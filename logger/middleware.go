6ad8f8acee8786b0082ac7e9655f966d758eb018
kumnp443 <kumneung.p@entronica.co.th>
2024-01-19 11:05:55 +0700

    Add MAC and IP address information to logger middleware

--- logger/middleware.go
+++ logger/middleware.go
@@ -2,14 +2,17 @@ package logger
 
 import (
 	"context"
+	"net"
 	"net/http"
+	"strings"
 
 	"github.com/google/uuid"
 	"github.com/gorilla/mux"
 	"go.uber.org/zap"
 )
 
-const key = "logger"
+type contextKey string
+const key contextKey = "logger"
 
 func Middleware(logger ILogger) mux.MiddlewareFunc {
 	return func(next http.Handler) http.Handler {
@@ -32,9 +35,68 @@ func LogParentID(r *http.Request, logger ILogger) ILogger {
 	}
 	xSpan := uuid.NewString()
 
-	return logger.With(zap.String("parent-id", xParent), zap.String("span-id", xSpan))
+	return logger.With(zap.Any("headers", GetHeaders(r)), zap.String("parent-id", xParent), zap.String("span-id", xSpan))
 }
 
 func setLoggerContext(r *http.Request, val ILogger) *http.Request {
 	return r.WithContext(context.WithValue(r.Context(), key, val))
+}
+
+func getMACAndIP() MacIP {
+	interfaces, _ := net.Interfaces()
+	macAddr := MacIP{}
+	for _, iface := range interfaces {
+
+		if iface.Name != "" {
+			macAddr.InterfaceName = iface.Name
+		}
+
+		if iface.HardwareAddr != nil {
+			macAddr.HardwareAddr = iface.HardwareAddr.String()
+		}
+
+		var ips []string
+		addrs, _ := iface.Addrs()
+
+		for _, addr := range addrs {
+			ips = append(ips, addr.String())
+		}
+
+		if len(ips) > 0 {
+			macAddr.IPs = ips
+		}
+	}
+
+	return macAddr
+}
+
+type MacIP struct {
+	InterfaceName string   `json:"interface_name"`
+	HardwareAddr  string   `json:"hardware_addr"`
+	IPs           []string `json:"ips"`
+}
+
+func GetHeaders(r *http.Request) map[string]interface{} {
+	// Request user agent
+	userAgent := r.UserAgent()
+	platform := strings.Split(r.Header.Get("sec-ch-ua"), ",")
+	mobile := r.Header.Get("sec-ch-ua-mobile")
+	operatingSystem := r.Header.Get("sec-ch-ua-platform")
+	clientIP := r.RemoteAddr
+	reqId := r.Header.Get("X-Session-Id")
+	if reqId == "" {
+		reqId = uuid.NewString()
+	}
+
+	macIp := getMACAndIP()
+	return map[string]interface{}{
+		"user_agent": userAgent,
+		"Platform":   platform,
+		"Mobile":     mobile,
+		"OS":         operatingSystem,
+		"client_ip":  clientIP,
+		"request_id": reqId,
+		"remote_ip":  r.RemoteAddr,
+		"mac_ip":     macIp,
+	}
 }
